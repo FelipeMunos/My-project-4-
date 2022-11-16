56270992799c5c0e885cd765a2c1824e1e0e8cba
Felipe <luis.munoz209@inacapmail.cl>
2022-11-16 08:01:16 -0300

    oo

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int PuntosTotales {get{return puntosTotales;}}
+    private int puntosTotales
+    public void SumarPuntos(int puntosaSumar){
+        puntosTotales = PuntosTotales + puntosaSumar
+        Debug.Log(puntosTotales)
+    }
     // Start is called before the first frame update
     void Start()
     {
